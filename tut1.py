6b484d6107189f2c7088f90093af933a901ab795
Noor Saeed <124183389+611noorsaeed@users.noreply.github.com>
2023-11-09 06:19:54 -0800

    Add files via upload

---
+++ tut1.py
@@ -0,0 +1,40 @@
+# 1: Print statement in python which prints things....
+# print("This is my first program in python")
+# print('Helo friend')
+
+#=====================================
+# 2 Variables (containers store data (values))
+# name = "Noor Saeed" # sting vlaues
+# age = 25  # integer values
+# salary = 10.2 # double or float values
+# position = '+' # character (single charact spe(-,+%$)
+# print("this is name : ", name)
+# print("this is age: ", age)
+# print("This is salary: ", salary)
+# print("position", position)
+
+#===========================================
+# 3 user input (input statement)
+# name = input('Enter your name,')
+# print(name)
+# age = int(input("enter your age,"))
+# print(age)
+# salary = float(input("Enter your salary,"))
+# print(salary)
+# pos = input("Etner  position ")
+# print(pos)
+
+# 4 practice =======
+# login system
+email = input("Enter your email...")
+password = int(input("Enter you password"))
+# print(email, " ", password)
+if email and password:
+    print("you are logedin successfully...")
+
+
+
+
+
+
+
